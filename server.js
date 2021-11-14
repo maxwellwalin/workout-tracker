9ccc8c79f17fa7d0b088371e6222049f763df7da
maxwell walin <maxwellwalin@gmail.com>
2021-11-14 11:18:43 -0800

    deploying

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutTracker", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
